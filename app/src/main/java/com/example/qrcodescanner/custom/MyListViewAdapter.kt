d55618ce485e6a012a10bf98f152781665b5a0d9
Lenovo <raintruwalker@gmail.com>
2023-04-07 08:48:12 +0300

    Android version 17

--- app/src/main/java/com/example/qrcodescanner/custom/MyListViewAdapter.kt
+++ app/src/main/java/com/example/qrcodescanner/custom/MyListViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.qrcodescanner.custom
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +12,7 @@ import com.example.qrcodescanner.R
 class MyListViewAdapter(private val context: Activity, private val arrayList: ArrayList<User>) :
     ArrayAdapter<User>(context, R.layout.list_item, arrayList) {
 
+    @SuppressLint("ViewHolder", "InflateParams")
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater: LayoutInflater = LayoutInflater.from(context)
         val view: View = inflater.inflate(R.layout.list_item, null)
